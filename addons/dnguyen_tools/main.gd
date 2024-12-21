4bc8e64b5090f85f1a27fa09e64e7bd2c543137a
Dytrich NguyeN <nguyen.dytrich@gmail.com>
2024-12-20 21:46:12 -0800

    Add single subviewport, scaffold generation opts

--- addons/dnguyen_tools/main.gd
+++ addons/dnguyen_tools/main.gd
@@ -3,21 +3,64 @@ extends Control
 
 @export var btn: Button
 @export var tree: Tree
+@export var sub_viewport: SubViewport
 const MAX_RECURSION: int = 100
 
 @onready var fs = EditorInterface.get_resource_filesystem().get_filesystem()
 
 var obj_files: Dictionary = {}
 
+var selected_file: String = ""
+var mesh: MeshInstance3D
+var delta: float = 0.0
+
 
 func _ready():
-	_reset_tree()
 	btn.pressed.connect(_on_button_pressed)
+	sub_viewport.get_parent().gui_input.connect(_handle_sub_vp_gui_input)
 
-func _on_button_pressed():
 	_reset_tree()
 	_scan_filesystem(fs)
 	_render_ui()
+
+
+# TODO: Fix this jank
+func _handle_sub_vp_gui_input(event: InputEvent) -> void:
+	if not mesh:
+		return
+	if event is InputEventMouseMotion:
+		if event.button_mask == MOUSE_BUTTON_MASK_LEFT:
+			mesh.global_basis = mesh.global_basis.rotated(Vector3.UP, event.relative.x * delta)
+			mesh.global_basis = mesh.global_basis.rotated(Vector3.LEFT, event.relative.y * delta)
+			sub_viewport.get_camera_3d().look_at(mesh.get_aabb().get_center())
+
+func _process(d: float) -> void:
+	delta = d
+	var selected = tree.get_selected()
+	if selected and selected.get_text(0).match("*.obj"):
+		var fname = selected.get_text(0)
+		var path = "res://%s/%s" % [obj_files[fname], fname]
+		if path != selected_file:
+			selected_file = path
+			_clear_sub_viewport()
+			_render_to_sub_viewport(path)
+
+# Add to export
+func _on_button_pressed():
+	pass
+
+func _render_to_sub_viewport(file: String):
+	var array_mesh: ArrayMesh = load(file)
+	mesh = MeshInstance3D.new()
+	mesh.mesh = array_mesh
+	sub_viewport.add_child(mesh)
+	sub_viewport.get_camera_3d().look_at(mesh.get_aabb().get_center())
+
+
+func _clear_sub_viewport():
+	if mesh:
+		mesh.queue_free()
+	mesh = null
 
 func _parse_obj_files_to_dict() -> Dictionary:
 	var tree_repr: Dictionary = {}
